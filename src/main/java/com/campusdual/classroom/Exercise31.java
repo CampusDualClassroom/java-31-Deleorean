ae59968b79fb29bf59a9a9cbc1c664baf91541d1
Edugonzalez <eduu.gonzalez@gmail.com>
2025-02-06 20:14:54 +0100

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,29 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
-
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("src/main/resources/lorem.txt"));
+            String linea = "";
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
     }
 }
+
