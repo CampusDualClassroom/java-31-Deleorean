613816fca27565f70a6dcb177f709755cf6eccf3
Edugonzalez <eduu.gonzalez@gmail.com>
2025-02-11 13:52:59 +0100

    Refactorizado a un try-with-resources

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -6,7 +6,8 @@ import java.io.FileReader;
 public class Exercise31 {
 
     public static void main(String[] args) {
-        BufferedReader br = null;
+      /* Try-catch normal con fiunally
+       BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader("src/main/resources/lorem.txt"));
             String linea = "";
@@ -23,6 +24,16 @@ public class Exercise31 {
             } catch (Exception e2) {
                 e2.printStackTrace();
             }
+        }*/
+
+        //Try with resources
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/lorem.txt"))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
